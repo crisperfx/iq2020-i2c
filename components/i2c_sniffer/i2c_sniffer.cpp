685e0817f484c34d954bbaa3af656b072b2f0d87
crisperfx <dempsydegrande@gmail.com>
2025-06-15 15:17:37 +0200

    Update and rename iq2020_sniffer.cpp to i2c_sniffer.cpp

--- components/i2c_sniffer/i2c_sniffer.cpp
+++ components/i2c_sniffer/i2c_sniffer.cpp
@@ -2,25 +2,24 @@
 #include "esphome/core/log.h"
 
 namespace esphome {
-namespace iq2020_sniffer {
+namespace i2c_sniffer {
 
-static const char *const TAG = "iq2020.sniffer";
+static const char *const TAG = "i2c_sniffer";
 
 #define SDA_PIN 25
 #define SCL_PIN 21
 
-void IQ2020Sniffer::setup() {
-  ESP_LOGI(TAG, "Sniffer started (SDA=%d, SCL=%d)", SDA_PIN, SCL_PIN);
+void I2CSniffer::setup() {
+  ESP_LOGI(TAG, "Sniffer setup complete");
   pinMode(SDA_PIN, INPUT_PULLUP);
   pinMode(SCL_PIN, INPUT_PULLUP);
 }
 
-void IQ2020Sniffer::loop() {
+void I2CSniffer::loop() {
   bool sda = digitalRead(SDA_PIN);
   bool scl = digitalRead(SCL_PIN);
   ESP_LOGD(TAG, "SDA: %d, SCL: %d", sda, scl);
-  delay(5);
 }
 
-}  // namespace iq2020_sniffer
+}  // namespace i2c_sniffer
 }  // namespace esphome
