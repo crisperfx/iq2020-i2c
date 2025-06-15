e5b1e4fc053757567c6ba4bee0e7625e6e929760
crisperfx <dempsydegrande@gmail.com>
2025-06-15 17:12:33 +0200

    Update i2c_sniffer.h

--- components/i2c_sniffer/i2c_sniffer.h
+++ components/i2c_sniffer/i2c_sniffer.h
@@ -7,6 +7,18 @@ class I2CSniffer : public Component {
  public:
   void setup() override;
   void loop() override;
+
+ private:
+  // Houd vorige pinwaarden bij
+  bool prev_sda = true;
+  bool prev_scl = true;
+
+  // Status flags en buffers
+  bool receiving = false;
+  bool receiving_address = true;
+  bool ack_bit_expected = false;
+  uint8_t bit_count = 0;
+  uint8_t byte_buf = 0;
 };
 
 }  // namespace i2c_sniffer
