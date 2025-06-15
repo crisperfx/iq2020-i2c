0d0e4f9a7f3648c797bee6c88e7501f0c7bfd299
crisperfx <dempsydegrande@gmail.com>
2025-06-15 15:33:13 +0200

    Update i2c_sniffer.cpp

--- components/i2c_sniffer/i2c_sniffer.cpp
+++ components/i2c_sniffer/i2c_sniffer.cpp
@@ -10,9 +10,10 @@ static const char *const TAG = "i2c_sniffer";
 #define SCL_PIN 21
 
 void I2CSniffer::setup() {
-  ESP_LOGI(TAG, "Sniffer setup complete");
+  ESP_LOGI(TAG, "Sniffer setup START");
   pinMode(SDA_PIN, INPUT_PULLUP);
   pinMode(SCL_PIN, INPUT_PULLUP);
+  ESP_LOGI(TAG, "Sniffer setup COMPLETE");
 }
 
 void I2CSniffer::loop() {
