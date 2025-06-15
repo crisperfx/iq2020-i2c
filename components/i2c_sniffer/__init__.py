2f21774ce65d35f1affaa3a6271484fe721f555a
crisperfx <dempsydegrande@gmail.com>
2025-06-15 18:35:38 +0200

    Update __init__.py

--- components/i2c_sniffer/__init__.py
+++ components/i2c_sniffer/__init__.py
@@ -1,16 +1,32 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
+from esphome import automation
+from esphome.components import logger
+from esphome.const import CONF_ID, CONF_SDA_PIN, CONF_SCL_PIN, CONF_LOG_LEVEL, CONF_BUFFER_SIZE, CONF_LATITUDE, CONF_LONGITUDE
 
-i2c_sniffer_ns = cg.esphome_ns.namespace("i2c_sniffer")
-I2CSniffer = i2c_sniffer_ns.class_("I2CSniffer", cg.Component)
+# Define namespace for your component
+i2c_sniffer_ns = cg.esphome_ns.namespace('i2c_sniffer')
+I2CSniffer = i2c_sniffer_ns.class_('I2CSniffer', cg.Component)
 
-CONF_I2C_SNIFFER_ID = "i2c_sniffer_id"
+# Configuration options
+CONF_SDA_PIN = "sda_pin"
+CONF_SCL_PIN = "scl_pin"
+CONF_LOG_LEVEL = "log_level"
+CONF_BUFFER_SIZE = "buffer_size"
 
+# Define the schema
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(CONF_I2C_SNIFFER_ID): cv.declare_id(I2CSniffer),
-}).extend({})
+    cv.GenerateID(): cv.declare_id(I2CSniffer),
+    cv.Required(CONF_SDA_PIN): cv.int_,
+    cv.Required(CONF_SCL_PIN): cv.int_,
+    cv.Optional(CONF_LOG_LEVEL, default=2): cv.int_,  # 0=ERROR,1=WARN,2=INFO,3=DEBUG
+    cv.Optional(CONF_BUFFER_SIZE, default=0): cv.int_,
+}).extend(cv.COMPONENT_SCHEMA)
 
+# Generate code based on config
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_I2C_SNIFFER_ID])
-    await cg.register_component(var, config)
+    sniffer = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(sniffer, config)
+    cg.add(sniffer.set_pins(config[CONF_SDA_PIN], config[CONF_SCL_PIN]))
+    cg.add(sniffer.set_log_level(config[CONF_LOG_LEVEL]))
+    cg.add(sniffer.set_buffer_size(config[CONF_BUFFER_SIZE]))
