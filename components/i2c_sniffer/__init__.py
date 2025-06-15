0e35066080dceb16af313e97a8c395bf51101e16
crisperfx <dempsydegrande@gmail.com>
2025-06-15 15:35:45 +0200

    Update __init__.py

--- components/i2c_sniffer/__init__.py
+++ components/i2c_sniffer/__init__.py
@@ -1,11 +1,11 @@
 import esphome.codegen as cg
+import esphome.config_validation as cv
 
 i2c_sniffer_ns = cg.esphome_ns.namespace('i2c_sniffer')
 I2CSniffer = i2c_sniffer_ns.class_('I2CSniffer', cg.Component)
 
-CONFIG_SCHEMA = cg.Schema({}).extend({})
+CONFIG_SCHEMA = cv.Schema({}).extend({})
 
 async def to_code(config):
-    var = cg.new_Pvariable("sniffer", I2CSniffer())
+    var = cg.new_Pvariable("i2c_sniffer", I2CSniffer())
     await cg.register_component(var, config)
-
